ea7012d9742bbf1870c49b48f16d784f1696b224
huanyu.yang <mfuture@qq.com>
2022-11-16 08:40:39 +0000

    add validation for StreamPushDTO

--- src/app/dto/stream/push.ts
+++ src/app/dto/stream/push.ts
@@ -9,9 +9,19 @@ export class StreamPushDTO {
   @Rule(
     RuleType.object({
       type: RuleType.string().trim().required(),
-      file: RuleType.object().optional(),
-      mediasoup: RuleType.object().optional(),
-    }))
+      file: RuleType.object({
+        format: RuleType.string().valid('pcm', 'opus', 'mp3').required(),
+        name: RuleType.string().valid('file1', 'file2').required(),
+      }
+      ).optional(),
+      mediasoup: RuleType.object(
+        {
+          format: RuleType.string().valid('pcm', 'opus', 'mp3').required(),
+          room: RuleType.string().valid('room1', 'room2').required(),
+          user: RuleType.string().valid('user1', 'user2').required(),
+        }
+      ).optional(),
+    }).required())
   stream: object;
 
   @CreateApiPropertyDoc('腾讯 AsrSDK 配置')
@@ -26,7 +36,7 @@ export class StreamPushDTO {
 /**
  * 获取单个管理员参数
  */
- export class ShowDTO {
+export class ShowDTO {
   @CreateApiPropertyDoc('管理员的id')
   @Rule(RuleType.string().trim().max(10).required())
   id: string;
