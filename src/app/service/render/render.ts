757b86e6b635f67e3ca4e143bd2d6ae247cb1c22
huanyu.yang <mfuture@qq.com>
2022-12-13 19:50:42 +0800

    ok for filter graph withwout relationship check

--- src/app/service/render/render.ts
+++ src/app/service/render/render.ts
@@ -1,9 +1,9 @@
 
-import { Provide, Inject } from '@midwayjs/decorator';
+import { Provide, Inject, App } from '@midwayjs/decorator';
 import { InjectEntityModel } from '@midwayjs/orm';
 import { Repository } from 'typeorm';
 
-import { Context } from '@/interface';
+import { Application, Context } from '@/interface';
 
 import { AdminUserModel } from '../../model/admin-user';
 
@@ -15,6 +15,9 @@ const TWENTYFOURHOURS = 24 * 60 * 60;
 export class RenderService {
   @Inject()
   ctx: Context;
+
+  @App()
+  private _app!: Application;
 
   @InjectEntityModel(AdminUserModel)
   adminUserModel: Repository<AdminUserModel>;
@@ -28,22 +31,15 @@ export class RenderService {
 
   //初始化模板
   async initTemplate(params: any) {
-    const regions = params.template.regions;
-    const filters = params.filters;
-    const srcs = params.srcs;
-    const src2Regions = params.src2Regions;
 
-    //初始化滤波器
-    filters.forEach(filter2Region => {
-      const theRegion = regions.find(region => region.id === filter2Region.regionId);
-      theRegion.filters = filter2Region.filters;
-    });
+    //区域不包含 drawtext 和 硬字幕 subtitles
+    const regions = params.template.regions.filter(item => ['video', 'audio', 'picture'].indexOf(item.type) > -1);
+    const srcs = params.srcs;
 
     //初始化数据源
-    src2Regions.forEach(src2Region => {
-      const theRegion = regions.find(region => region.id === src2Region.regionId);
-      const theSrc = srcs.find(src => src.id === src2Region.srcId)
-      theRegion.src = theSrc;
+    regions.forEach(itemRegion => {
+      const theSrc = srcs.find(src => src.id === itemRegion.srcId)
+      itemRegion.src = theSrc;
     });
 
     //存储初始化后的模板
@@ -54,10 +50,13 @@ export class RenderService {
     //存储数据源，和 template 匹配
     await this.ctx.app.redis.set(key + "_srcs", JSON.stringify(params.srcs), 'EX', TWENTYFOURHOURS);
 
-    this.initFilterGraph(template)
+    const t1 = Date.now();
 
-    const result = await this.ctx.app.redis.get(key);
-    return JSON.parse(result);
+    const filterDesc = this.__initFilterGraph(template); // 耗时小于 10 ms
+
+    console.log(`--- init filter graph: ${Date.now() - t1} ms`);
+
+    return filterDesc;
   }
 
   //数据源到模板区域的映射
@@ -122,19 +121,22 @@ export class RenderService {
 
 
   /**通过模板及其参数构造滤波器图 */
-  async initFilterGraph(template: any) {
+  private async __initFilterGraph(template: any) {
     console.log('============= start to init filter graph ============');
 
-    // const filter = `movie=/opt/application/tx-rtcStream/files/resources/${params.img0}[m0];movie=/opt/application/tx-rtcStream/files/resources/mask.png[m1];movie=/opt/application/tx-rtcStream/files/resources/${params.video2}[m2];movie=/opt/application/tx-rtcStream/files/resources/${params.img3}[m3];movie=/opt/application/tx-rtcStream/files/resources/${params.video4}[m4];[m0]crop=200:200:200:200[cropped1];[m1]alphaextract[amask];[amask]scale=150:150[vmask];[m2]scale=150:150[cropped3];[cropped3][vmask]alphamerge[avatar];[in][cropped1]overlay=W-w-10:10[ov1];[ov1][avatar]overlay=100:10[ov2];[m3]scale=50:50[gif];[ov2][gif]overlay=W-w-10:H/2[ov3];[m4]scale=200:300,chromakey=0x00ff00:0.3:0.05[ov4];[ov3][ov4]overlay=${params.dh.x}:${params.dh.y}[ov5];[ov5]subtitles=/opt/application/tx-rtcStream/files/resources/subtitles.srt[final];[final]drawtext=text=${params.drawtext.text}:fontfile=/usr/share/fonts/chinese/SIMKAI.TTF:x=${params.drawtext.x}:y=${params.drawtext.y}:fontcolor=${params.drawtext.color}:fontsize=${params.drawtext.fontsize}:shadowx=2:shadowy=2`
+    this.__checkTemplate(template);
 
-    const regions = template.regions as Array<any>;
+    // const filter = `movie=/opt/application/tx-rtcStream/files/resources/fileimage.png[m0];movie=/opt/application/tx-rtcStream/files/resources/mask.png[m1];movie=/opt/application/tx-rtcStream/files/resources/dh.mp4[m2];movie=/opt/application/tx-rtcStream/files/resources/fileimage.png[m3];movie=/opt/application/tx-rtcStream/files/resources/dh.mp4[m4];[m0]crop=100:50:200:200[cropped1];[m1]alphaextract[amask];[amask]scale=150:150[vmask];[m2]scale=150:150[cropped3];[cropped3][vmask]alphamerge[avatar];[in][cropped1]overlay=W-w-10:10[ov1];[ov1][avatar]overlay=100:10[ov2];[m3]scale=50:50[gif];[ov2][gif]overlay=W-w-10:H/2[ov3];[m4]scale=200:300,chromakey=0x00ff00:0.3:0.05[ov4];[ov3][ov4]overlay=-20+5*n:H*0.2[ov5];[ov5]subtitles=/opt/application/tx-rtcStream/files/resources/subtitles.srt[final];[final]drawtext=textfile=/opt/application/tx-rtcStream/files/resources/drawtext.txt:reload=1:fontfile=/usr/share/fonts/chinese/SIMKAI.TTF:x=(w-text_w)/2:y=h-80*t:fontcolor=white:fontsize=40:shadowx=2:shadowy=2`
+
+    // const regions = template.regions as Array<any>;
+
+    const regions = template.regions.filter(item => ['video', 'audio', 'picture'].indexOf(item.type) > -1) as Array<any>;
+
 
     //第一步：把数据源写入滤波器 movie, 作为文件输入使用
     const inputs = regions.map(region => {
       return `movie=${region.src.path}[${region.id}]`
     });
-    console.log('---inputs');
-    console.log(inputs);
 
     //第二步：对不同的区域数据源使用 filter-chain
     let regionsNameAfterFilter = [];
@@ -164,8 +166,6 @@ export class RenderService {
       regionsNameAfterFilter.push(region.id + '_filt');
       return `[${region.id}]` + filterDesc.join(",") + `[${region.id}_filt]`;
     });
-    console.log('---filterChains');
-    console.log(filterChains);
 
     //第三步：按照 layer(regionId 中的 z 数据) 关系进行 overlay
     const regionsSorted = regionsNameAfterFilter.sort((a, b) => {
@@ -173,8 +173,7 @@ export class RenderService {
       const layerB = b.split('_')[1].split('.')[0];
       return Number(layerA) - Number(layerB);
     })
-    console.log('---regionsSorted');
-    console.log(regionsSorted);
+
     const regionsToOverlay = regionsSorted.filter(regionId => {
       const index = regionId.indexOf('_filt');
       let regionIdTrue = '';
@@ -187,8 +186,11 @@ export class RenderService {
       return theRegion;
     })
 
+    // [in][region0.0.0]overlay=0:0[out0];
+
     let overlays = [];
-    for (let i = 1; i < regionsToOverlay.length; i++) {
+    let lastFilterTag = '';// 标记最后一个输出 
+    for (let i = 0; i < regionsToOverlay.length; i++) {
       const regionLabel = regionsToOverlay[i];
       const index = regionLabel.indexOf('_filt');
       let regionId = '';
@@ -207,23 +209,48 @@ export class RenderService {
       const x = theRegion.area.x;
       const y = theRegion.area.y;
       let desc = '';
-      if (i === 1) {
-        desc = `[${regionsToOverlay[0]}][${regionLabel}]overlay=${x}:${y}[out1]`;
-      } else if (i === regionsToOverlay.length - 1) {
-        desc = `[out${i - 1}][${regionLabel}]overlay=${x}:${y}`;
+      if (i === 0) {
+        desc = `[in][${regionLabel}]overlay=${x}:${y}[out0]`; //对应于 c 代码中添加的 [in] 标记
       } else {
         desc = `[out${i - 1}][${regionLabel}]overlay=${x}:${y}[out${i}]`;
       }
+      lastFilterTag = `out${i}`;
       overlays.push(desc);
     }
     console.log('---overlays');
     console.log(overlays);
 
-    //组合所有 filter-chain
-    const filterGraph = [].concat(...inputs).concat(...filterChains).concat(...overlays).join(';');
+    //第四步：从 regions 中取出 drawtext 和 subtitles(硬字幕) 作为全局的滤波器 
+    const textFilters = template.regions.filter(item => item.type === 'subtitles' || item.type === 'drawtext') as Array<any>;
+    textFilters.sort((a, b) => {
+      const layerA = a.id.split('_')[1].split('.')[0];
+      const layerB = b.id.split('_')[1].split('.')[0];
+      return Number(layerA) - Number(layerB);
+    });
 
-    console.log('--- filter graph');
-    console.log(filterGraph);
+
+    const textFiltersSorted = textFilters.map(itemFilter => {
+      if (!itemFilter.options) {
+        return `${itemFilter.type}`;
+      }
+
+      let filterStr = `${itemFilter.type}=`;
+      let propertys = []
+      for (let [key, val] of Object.entries(itemFilter.options)) {
+        propertys.push(`${key}=${val}`);
+      }
+
+      let str = `[${lastFilterTag}]` + filterStr + (propertys.join(':'))
+      lastFilterTag = `outText_${itemFilter.id}`;
+      str += `[${lastFilterTag}]`;
+      return str;
+    })
+
+
+    //组合所有 filter-chain
+    const filterGraph = [].concat(...inputs, ...filterChains, ...overlays, ...textFiltersSorted).filter(item => item !== '').join(';');
+
+    await this.__writeFilterGraphIntoFile(filterGraph)
 
     console.log('------- end of  filter graph init --------');
 
@@ -231,4 +258,30 @@ export class RenderService {
     return filterGraph;
   }
 
+  /**模板有效性检验 */
+  private __checkTemplate(template: any) {
+    for (let [key, val] of Object.entries(template.profile)) {
+      const count = template.regions.filter(item => item.type === key).length;
+      if (count !== val) {
+        console.error(`--- the template failed at ${key} check`)
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**写入服务器文件 */
+  private async __writeFilterGraphIntoFile(filterGraph: string) {
+    const serverHttp = "https://cosmoserver.tk:4443/stream/render"
+    const result = await this._app.curl(serverHttp, {
+      method: 'POST',
+      data: { text: filterGraph },
+      dataType: 'json',
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+
+    return result;
+  }
 }
