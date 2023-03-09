8b40a71565779b6f93dcf7a6d3e6688531e50c76
huanyu.yang <mfuture@qq.com>
2023-03-09 11:22:59 +0800

    add assets

--- src/app/controller/assets/index.ts
+++ src/app/controller/assets/index.ts
@@ -91,6 +91,10 @@ const category = [{
       title: "images",
       key: "0-0-3",
     },
+    {
+      title: "subtitles",
+      key: "0-0-4",
+    },
   ],
 }]
 
@@ -100,40 +104,110 @@ const category = [{
 const assets = {
   videos: [
     {
+      id: 1,
       previewImage: 'https://www.baidu.com/img/bd_logo1.png',
       url: 'https://chaosyhy.com:60125/files/resources/40_input.mp4',
-      title: '40s 时钟视频'
+      path: '/opt/application/tx-rtcStream/files/resources/40_input.mp4',
+      title: '40s时钟视频'
     },
     {
+      id: 2,
       previewImage: 'https://www.baidu.com/img/bd_logo1.png',
       url: 'https://chaosyhy.com:60125/files/resources/forest.mp4',
+      path: '/opt/application/tx-rtcStream/files/resources/forest.mp4',
       title: '森林'
-    }
+    },
+    {
+      id: 3,
+      previewImage: 'https://www.baidu.com/img/bd_logo1.png',
+      url: 'https://chaosyhy.com:60125/files/resources/20_input.mp4',
+      path: '/opt/application/tx-rtcStream/files/resources/20_input.mp4',
+      title: '20s时钟视频'
+    },
+    {
+      id: 4,
+      previewImage: 'https://www.baidu.com/img/bd_logo1.png',
+      url: 'https://chaosyhy.com:60125/files/resources/video2.mp4',
+      path: '/opt/application/tx-rtcStream/files/resources/video2.mp4',
+      title: '电脑屏幕'
+    },
+    {
+      id: 5,
+      previewImage: 'https://www.baidu.com/img/bd_logo1.png',
+      url: 'https://chaosyhy.com:60125/files/resources/dh.mp4',
+      path: '/opt/application/tx-rtcStream/files/resources/dh.mp4',
+      title: '本地数字人'
+    },
+    {
+      id: 6,
+      previewImage: 'https://www.baidu.com/img/bd_logo1.png',
+      url: 'https://chaosyhy.com:60125/files/resources/testsrc_1280x960_30p.mp4',
+      path: '/opt/application/tx-rtcStream/files/resources/testsrc_1280x960_30p.mp4',
+      title: '1280P时钟'
+    },
 
   ],
   images: [
     {
+      id: 1,
       previewImage: 'https://chaosyhy.com:60125/files/resources/fileimage.png',
       url: 'https://chaosyhy.com:60125/files/resources/fileimage.png',
+      path: '/opt/application/tx-rtcStream/files/resources/fileimage.jpg',
       title: '流控制器'
     },
     {
+      id: 2,
       previewImage: 'https://chaosyhy.com:60125/files/resources/image1.jpg',
       url: 'https://chaosyhy.com:60125/files/resources/image1.jpg',
+      path: '/opt/application/tx-rtcStream/files/resources/image1.jpg',
       title: '拉萨'
+    },
+    {
+      id: 3,
+      previewImage: 'https://alifei05.cfp.cn/creative/vcg/veer/1600water/veer-412747764.jpg',
+      url: 'https://alifei05.cfp.cn/creative/vcg/veer/1600water/veer-412747764.jpg',
+      path: 'https://alifei05.cfp.cn/creative/vcg/veer/1600water/veer-412747764.jpg',
+      title: '网络图片'
+    },
+    {
+      id: 4,
+      previewImage: 'https://chaosyhy.com:60125/files/resources/image2.jpg',
+      url: 'https://chaosyhy.com:60125/files/resources/image2.jpg',
+      path: '/opt/application/tx-rtcStream/files/resources/image2.jpg',
+      title: '深山'
+    },
+    {
+      id: 5,
+      previewImage: 'https://chaosyhy.com:60125/files/resources/gif.gif',
+      url: 'https://chaosyhy.com:60125/files/resources/gif.gif',
+      path: '/opt/application/tx-rtcStream/files/resources/gif.gif',
+      title: '动态图'
     }
   ],
   audios: [
     {
+      id: 1,
       previewImage: 'https://chaosyhy.com:60125/files/resources/fileimage.png',
       url: 'https://chaosyhy.com:60125/files/resources/彩虹.mp3',
+      path: '/opt/application/tx-rtcStream/files/resources/彩虹.mp3',
       title: '周杰伦-彩虹'
     },
     {
+      id: 2,
       previewImage: 'https://chaosyhy.com:60125/files/resources/fileimage.png',
       url: 'https://chaosyhy.com:60125/files/resources/成都.mp3',
+      path: '/opt/application/tx-rtcStream/files/resources/成都.mp3',
       title: '赵雷-成都'
     }
+  ],
+  subtitles: [
+    {
+      id: 1,
+      previewImage: 'https://chaosyhy.com:60125/files/resources/fileimage.png',
+      url: 'https://chaosyhy.com:60125/files/resources/subtitles.srt',
+      path: '/opt/application/tx-rtcStream/files/resources/subtitles.srt',
+      title: '字幕'
+    },
   ],
   rtc: [
     {
