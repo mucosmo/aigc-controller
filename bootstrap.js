4b3b8066c8500bab41cb08b7855705506d05b21a
huanyu.yang <mfuture@qq.com>
2023-06-06 11:06:12 +0800

    change script for "npm start"

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,14 @@
 // 如果使用PM2启动这个项目，可以只需 `NODE_ENV=production node bootstrap.js`
 // 获取框架
 // eslint-disable-next-line import/order
+const dotenv =require('dotenv');
+dotenv.config();
+
 const WebFramework = require('@midwayjs/web').Framework;
 // 初始化 web 框架并传入启动参数
+
 const web = new WebFramework().configure({
-  port: 9000,
+  port: process.env.SERVER_PORT,
   hostname: '0.0.0.0',
 });
 
